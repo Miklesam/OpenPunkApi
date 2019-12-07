d4990c5b0f04e01c7dbae3c4bee13984c2fce2fd
Miklesami <mikle.samarkin@gmail.com>
2019-12-07 13:45:10 +0300

    init

---
+++ app/src/main/java/com/miklesam/openpunkapi/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.miklesam.openpunkapi.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
